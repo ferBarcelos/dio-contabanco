f5927aa061986909b2dd39ea3012e67babad13f6
ferBarcelos <serve.barcelos@gmail.com>
2024-06-12 16:30:16 -0300

    Ajustes no Scanner

--- src/principal/ContaTerminal.java
+++ src/principal/ContaTerminal.java
@@ -8,19 +8,19 @@ public class ContaTerminal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+		Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
 		System.out.print("Por favor, digite seu Nome Completo: ");
-		String nomeCliente = scanner.next();
+		String nomeCliente = scan.next();
 
 		System.out.print("Por favor, digite o número da Agência: ");
-		String agencia = scanner.next();
+		String agencia = scan.next();
 
 		System.out.print("Por favor, digite o Número da Conta: ");
-		int numeroConta = scanner.nextInt();
+		int numeroConta = scan.nextInt();
 
 		System.out.print("Por favor, digite o Saldo: ");
-		double saldoConta = scanner.nextDouble();
+		double saldoConta = scan.nextDouble();
 
 		System.out.println(
 				"Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
